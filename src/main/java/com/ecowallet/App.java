bb23ed78736be35469ebafec502081a9973595a0
layslima11 <rocha.lima.lays0411@gmail.com>
2026-05-22 14:23:28 -0300

    feat: estrutura inicial da interface JavaFX

---
+++ src/main/java/com/ecowallet/App.java
@@ -0,0 +1,31 @@
+package com.ecowallet;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+            App.class.getResource("/view/telaPrincipal.fxml")
+        );
+
+        Scene scene = new Scene(
+            fxmlLoader.load()
+        );
+
+        stage.setTitle("EcoWallet");
+
+        stage.setScene(scene);
+
+        stage.show();
+    }
+    public static void main(String[] args) {
+
+        launch();
+    }
+}
---
+++ src/main/java/com/ecowallet/controller/TelaPrincipalController.java
@@ -0,0 +1,27 @@
+package com.ecowallet.controller;
+
+import javafx.fxml.FXML;
+
+public class TelaPrincipalController {
+
+    @FXML
+    public void adicionarTransacao() {
+
+        System.out.println("Botão funcionando!");
+
+    }
+
+     @FXML
+    public void adicionarReceita() {
+
+        System.out.println("Receita adicionada!");
+
+    }
+
+    @FXML
+    public void adicionarDespesa() {
+
+        System.out.println("Despesa adicionada!");
+
+    }
+}
