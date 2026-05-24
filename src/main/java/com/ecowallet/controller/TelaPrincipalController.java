406d4b1ae3cf5adabdd188931555842073bd5b27
layslima11 <rocha.lima.lays0411@gmail.com>
2026-05-24 12:42:15 -0300

    feat: finalização do sistema EcoWallet com JavaFX

--- src/main/java/com/ecowallet/controller/TelaPrincipalController.java
+++ src/main/java/com/ecowallet/controller/TelaPrincipalController.java
@@ -1,27 +1,88 @@
 package com.ecowallet.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class TelaPrincipalController {
 
     @FXML
-    public void adicionarTransacao() {
+    private TextField campoDescricao;
 
-        System.out.println("Botão funcionando!");
+    @FXML
+    private TextField campoValor;
+
+    @FXML
+    private ComboBox<String> comboTipo;
+
+    @FXML
+    private ComboBox<String> comboCategoria;
+
+    @FXML
+    private DatePicker campoData;
+
+    @FXML
+    private Label labelSaldo;
+
+    private double saldo = 0;
+
+    private ObservableList<String> transacoes =
+        FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+
+    comboTipo.getItems().clear();
+    comboCategoria.getItems().clear();
+
+    comboTipo.getItems().add("Receita");
+    comboTipo.getItems().add("Despesa");
+    comboCategoria.getItems().add("Alimentação");
+    comboCategoria.getItems().add("Transporte");
+    comboCategoria.getItems().add("Saúde");
+    comboCategoria.getItems().add("Lazer");
+    comboCategoria.getItems().add("Salário");
+    comboCategoria.getItems().add("Outros");
+
+}
+
+    public TelaPrincipalController() {
 
     }
 
-     @FXML
+    @FXML
     public void adicionarReceita() {
 
-        System.out.println("Receita adicionada!");
+        double valor = Double.parseDouble(
+            campoValor.getText().replace(",", ".")
+        );
 
+        saldo += valor;
+        transacoes.add(
+            campoDescricao.getText()
+        );
+        labelSaldo.setText(
+            "Saldo Total: R$ " + saldo
+        );
     }
 
     @FXML
     public void adicionarDespesa() {
 
-        System.out.println("Despesa adicionada!");
+        double valor = Double.parseDouble(
+            campoValor.getText().replace(",", ".")
+        );
+
+        saldo -= valor;
+        transacoes.add(
+           campoDescricao.getText()
+        );
+
+        labelSaldo.setText(
+            "Saldo Total: R$ " + saldo
+        );
 
     }
+
 }
